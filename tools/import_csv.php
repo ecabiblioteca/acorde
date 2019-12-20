b8489ae3a386e7bcdb404053d38daacc7a6fac92
Tiago Murakami <trmurakami@gmail.com>
2019-12-20 17:26:04 -0300

    Initial

--- tools/import_csv.php
+++ tools/import_csv.php
@@ -6,181 +6,104 @@ require 'inc/functions.php';
    
 while ($record = fgets(STDIN)) {
 
-    //print_r($record);
     $row = explode("\t", $record);
 
     foreach ($row as $key => $value) {
         if ($value == "000") {
-            $rowNum["EID"] = $key;
+            define("EID", $key);
         }
-    //     if ($value == "DOI") {
-    //         $rowNum["DOI"] = $key;
-    //     }
+        if ($value == "130") {
+            define("MEIODEEXPRESSAO", $key);            
+        }
         if ($value == "280") {
-            $rowNum["title"] = $key;            
+            define("TITLE", $key);            
         }
+        if ($value == "290") {
+            define("ALTERNATENAME", $key);            
+        }        
+        if ($value == "295") {
+            define("ISPARTOFTITLE", $key);            
+        }        
         if ($value == "020") {
-            $rowNum["Authors"] = $key;
-        }        
-    //     if ($value == "Source") {
-    //         $rowNum["sourceTitle"] = $key;
-    //     }
-    //     if ($value == "Research Area") {
-    //         $rowNum["about"] = $key;
-    //     }         
-    //     if ($value == "Volume") {
-    //         $rowNum["Volume"] = $key;
-    //     }
-    //     if ($value == "Issue") {
-    //         $rowNum["Issue"] = $key;
-    //     }        
-    //     if ($value == "Pages") {
-    //         $pages = explode("-", $key);
-    //         $rowNum["PageStart"] = $pages[0];
-    //         if (isset($pages[1])) {
-    //             $rowNum["PageEnd"] = $pages[1];
-    //         } else {
-    //             $rowNum["PageEnd"] = "N/D";
-    //         }            
-    //     }
-    //     if ($value == "Publication Date") {
-    //         $rowNum["year"] = $key;
-    //     }
-    //     if ($value == "Times Cited") {
-    //         $rowNum["citations"] = $key;
-    //     }
-       
-
-    //     // if ($value == "Language of Original Document") {
-    //     //     $rowNum["language"] = $key;
-    //     // }
-
-    //     // if ($value == "ISSN") {
-    //     //     $rowNum["ISSN"] = $key;
-    //     // }
-    //     // if ($value == "Publisher") {
-    //     //     $rowNum["Publisher"] = $key;
-    //     // }
-    //     // if ($value == "Abstract") {
-    //     //     $rowNum["Abstract"] = $key;
-    //     // }
-    //     // if ($value == "Funding Details") {
-    //     //     $rowNum["FundingDetails"] = $key;
-    //     // } 
-
-    //     // if ($value == "References") {
-    //     //     $rowNum["References"] = $key;
-    //     // }
-    //     // if ($value == "Author Keywords") {
-    //     //     $rowNum["AuthorKeywords"] = $key;
-    //     // }
-    //     // if ($value == "Index Keywords") {
-    //     //     $rowNum["IndexKeywords"] = $key;
-    //     // }
-    //     // if ($value == "Authors with affiliations") {
-    //     //     $rowNum["AuthorsWithAffiliations"] = $key;
-    //     // }
-    //     // if ($value == "Affiliations") {
-    //     //     $rowNum["Affiliations"] = $key;
-    //     // }
-    //     unset($pages);                                 
+            define("AUTHORS", $key);
+        }
+        if ($value == "340") {
+            define("DATEPUBLISHED", $key);
+        }
+        if ($value == "330") {
+            define("PUBLISHER", $key);
+        }
+        if ($value == "420") {
+            define("GENEROEFORMA", $key);            
+        }           
+                                
     }
-    //print_r($rowNum);
 
-
-    //while (($row = fgetcsv($record, 108192, "\t")) !== false) {
-        $doc = Record::Build($row, $rowNum);   
+    $doc = Record::Build($row);
+    if ($doc["doc"]["name"] == "280") {
+        $sha256 = null;
+    } else {
         $sha256 = hash('sha256', ''.$doc["doc"]["name"].''.$doc["doc"]["source_id"].'');
-        print_r($doc);
-        if (!is_null($sha256)) {
-            $result = Elasticsearch::update($sha256, $doc);
-        }        
-    //     print_r($result);
-    //     //print_r($doc["doc"]["source_id"]);
-    //     echo "<br/><br/><br/>";
-    //     flush();        
+    }
+    //print_r($doc);
+    if (!is_null($sha256)) {
+        $result = Elasticsearch::update($sha256, $doc);
+    }        
 
-    //}
 }
-
-//sleep(5);
-//echo '<script>window.location = \'result.php?filter[]=type:"Work"&filter[]=tag:"'.$_POST["tag"].'"\'</script>';
 
 class Record
 {
-    public static function build($row, $rowNum)
+    public static function build($row)
     {
 
-        $doc["doc"]["source_id"] = $row[$rowNum["EID"]];
+        $doc["doc"]["source_id"] = $row[EID];
 
-        $doc["doc"]["type"] = "Gravação";
         $doc["doc"]["source"] = "Base Sonora";
-        $doc["doc"]["name"] = str_replace('"', '', $row[$rowNum["title"]]);
+
+        if (empty($row[TITLE])) {
+            $doc["doc"]["type"] = "Disco";
+            $ispartoftitle = explode("$", $row[ISPARTOFTITLE]);
+            $doc["doc"]["name"] = str_replace('"', '', $ispartoftitle[0]);
+        } else {
+            $doc["doc"]["type"] = "Gravação";
+            $title = explode("$", $row[TITLE]);
+            $doc["doc"]["name"] = str_replace('"', '', $title[0]);
+        }
+
+        if (!empty($row[ALTERNATENAME])) {
+            $alternateName = explode("$", $row[ALTERNATENAME]);
+            $doc["doc"]["alternateName"] = str_replace('"', '', $alternateName[0]);
+        }
         
+        if (!empty($row[MEIODEEXPRESSAO])) {
+            $doc["doc"]["USP"]["meio_de_expressao"] = explode("|", $row[MEIODEEXPRESSAO]);
+        }
+
+        if (!empty($row[GENEROEFORMA])) {
+            $doc["doc"]["USP"]["about"]["genero_e_forma"] = explode("|", $row[GENEROEFORMA]);
+        }
         
-        
-        // $doc["doc"]["datePublished"] = $row[$rowNum["year"]];
+        $doc["doc"]["datePublished"] = $row[DATEPUBLISHED];
        
-        // $doc["doc"]["tag"][] = $tag;
-        // if ($row[$rowNum["DOI"]] != "n/a") {        
-        //     $doc["doc"]["doi"] = $row[$rowNum["DOI"]];
-        // }
-        // if (isset($rowNum["language"])) {
-        //     $doc["doc"]["language"] = $row[$rowNum["language"]];
-        // }        
-        // //$doc["doc"]["description"] = $row[$rowNum["Abstract"]];
-
-
-        // $doc["doc"]["tipo"] = "Article";
-
-        // $doc["doc"]["isPartOf"]["name"] = $row[$rowNum["sourceTitle"]];
-        // $doc["doc"]["isPartOf"]["volume"] = $row[$rowNum["Volume"]];
-        // $doc["doc"]["isPartOf"]["fasciculo"] = $row[$rowNum["Issue"]];
-        // $doc["doc"]["pageStart"] = $row[$rowNum["PageStart"]];
-        // if ($rowNum["PageEnd"] != "N/D") {
-        //     $doc["doc"]["pageEnd"] = $row[$rowNum["PageEnd"]];
-        // }        
-        // //$doc["doc"]["isPartOf"]["issn"] = $row[$rowNum["ISSN"]];
-        // //$doc["doc"]["publisher"]["organization"]["name"] = $row[$rowNum["Publisher"]];
-        // $doc["doc"]["metrics"]["source"] = "Web of Science";
-        // $doc["doc"]["metrics"]["citations"] = $row[$rowNum["citations"]];
-        // $aboutArray = explode(",", $row[$rowNum["about"]]);
-        // foreach ($aboutArray as $about) {
-        //     $doc["doc"]["about"][] = strtoupper($about);
-        // }              
-        //$doc["doc"]["scopus"]["references"] = $row[$rowNum["References"]];        
-        
-
-        // Agência de fomento
-        // $agencia_de_fomento_array = explode(";", $row[$rowNum["FundingDetails"]]);
-        // $i_funder = 0;
-        // foreach ($agencia_de_fomento_array as $funder) {
-        //     $funderArray = explode(",", $funder);
-        //     if (count($funderArray) > 2) {
-        //         $doc["doc"]["funder"][$i_funder]["projectNumber"] = $funderArray[0];
-        //         $doc["doc"]["funder"][$i_funder]["name"] = ''.$funderArray[2].' ('.$funderArray[1].')';
-        //     } elseif (count($funderArray) > 1) {
-        //         $doc["doc"]["funder"][$i_funder]["name"] = ''.$funderArray[1].' ('.$funderArray[0].')';
-        //     } else {
-        //         $doc["doc"]["funder"][$i_funder]["name"] = $funderArray[0];
-        //     }            
-        //     $i_funder++;
-        // }
-
-        // Palavras chave
-        // $palavras_chave_authors = explode(";", $row[$rowNum["AuthorKeywords"]]);
-        // $palavras_chave_scopus = explode(";", $row[$rowNum["IndexKeywords"]]);
-        // $doc["doc"]["palavras_chave"] = array_merge($palavras_chave_authors, $palavras_chave_scopus);
+        $doc["doc"]["publisher"]["organization"]["name"] = explode("|", $row[PUBLISHER]);
 
         // Autores
-        $authorsArray = explode("|", $row[$rowNum["Authors"]]);
-        $i_aut=0;
-        foreach ($authorsArray as $author) {
-            $doc["doc"]["author"][$i_aut]["person"]["name"] = $author;
-            $i_aut++;
+        if (!empty($row[AUTHORS])) {
+            $authorsArray = explode("|", $row[AUTHORS]);
+            $i_aut=0;
+            foreach ($authorsArray as $author) {
+                $doc["doc"]["author"][$i_aut]["person"]["name"] = $author;
+                $i_aut++;
+            }
+            unset($authorsArray);
+        } else {
+            unset($doc["doc"]["author"]);
         }
-        $doc["doc_as_upsert"] = true;
+        $doc["doc_as_upsert"] = true;        
         return $doc;
+        unset($doc);
+        unset($i_aut);
 
 
 
